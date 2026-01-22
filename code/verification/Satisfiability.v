e238c964d9dad73c80dff42f50a875aa12b42d0f
Julia Dijkstra <julia.dijkstra@technolution.nl>
2026-01-22 15:53:59 +0100

    Put `final_model` in `final_sat_refl`

--- code/verification/Satisfiability.v
+++ code/verification/Satisfiability.v
@@ -24,43 +24,37 @@ Proof.
     + now apply solve_aux_final in H.
 Qed.
 
-Lemma final_model: forall (m: PA) (f: CNF) (Hwf: WellFormed m f),
-  Final (state m f Hwf) -> Model m f.
-Proof.
-  unfold Model. unfold Final. unfold not. intros. destruct (f_eval m f) eqn:Heq.
-  - destruct b.
-    + reflexivity.
-    + exfalso. apply H. apply f_eval_false_iff in Heq as [c [Hc_in_f Hconflict]].
-      destruct (proj2 (find_conflict_exists_iff m f)) as [c_conflict Hfind_conflict]. now exists c.
-      destruct (split_last_decision m) as [[m_split l_split]|] eqn:Hsplit.
-      (* t_backtrack *)
-      * exists (state (m_split ++p ¬l_split) f (wf_backtrack m m_split f l_split Hsplit Hwf)).
-        pose proof (split_decomp m m_split l_split Hsplit) as [n_split [-> Hno_dec]].
-        apply (t_backtrack m_split n_split f c_conflict l_split Hwf).
-        -- now apply find_conflict_c_in_f in Hfind_conflict.
-        -- now apply find_conflict_conflicting in Hfind_conflict.
-        -- assumption.
-      (* t_fail *)
-      * exists fail. apply (t_fail m f c_conflict).
-        -- now apply find_conflict_c_in_f in Hfind_conflict.
-        -- now apply find_conflict_conflicting in Hfind_conflict.
-        -- now apply split_none_iff.
-  (* t_decide *)
-  - exfalso. apply H. apply undef_decision_exists in Heq as [l_decide Hfind_dec].
-    exists (state (m ++d l_decide) f (wf_decide _ _ _ Hfind_dec Hwf)).
-    pose proof (find_decision_decomp m f l_decide Hfind_dec) as [c_decide [Hfind_undef_l Hc_in_f]].
-    eapply (t_decide m f c_decide l_decide).
-    + left. now apply find_undef_l_in_c in Hfind_undef_l.
-    + assumption.
-    + now apply find_undef_l_undef in Hfind_undef_l.
-Qed.
-
 Theorem final_sat_refl: forall (f: CNF),
   (exists (m: PA) (Hwf: WellFormed m f),
     state [] f (initial_wf f) ==>* state m f Hwf /\ Final (state m f Hwf)) <-> Sat f.
 Proof.
   intros. split.
-  - intros [m [Hwf [Htrans Hfinal]]]. exists m. now apply final_model in Hfinal.
+  - intros [m [Hwf [Htrans Hfinal]]]. exists m. unfold Model. destruct (f_eval m f) eqn:Heq.
+    + destruct b.
+      * reflexivity.
+      * exfalso. apply Hfinal. apply f_eval_false_iff in Heq as [c [Hc_in_f Hconflict]].
+        destruct (proj2 (find_conflict_exists_iff m f)) as [c_conflict Hfind_conflict]. now exists c.
+        destruct (split_last_decision m) as [[m_split l_split]|] eqn:Hsplit.
+        (* t_backtrack *)
+        -- exists (state (m_split ++p ¬l_split) f (wf_backtrack m m_split f l_split Hsplit Hwf)).
+           pose proof (split_decomp m m_split l_split Hsplit) as [n_split [-> Hno_dec]].
+           apply (t_backtrack m_split n_split f c_conflict l_split Hwf).
+          ++ now apply find_conflict_c_in_f in Hfind_conflict.
+          ++ now apply find_conflict_conflicting in Hfind_conflict.
+          ++ assumption.
+        (* t_fail *)
+        -- exists fail. apply (t_fail m f c_conflict).
+          ++ now apply find_conflict_c_in_f in Hfind_conflict.
+          ++ now apply find_conflict_conflicting in Hfind_conflict.
+          ++ now apply split_none_iff.
+    (* t_decide *)
+    + exfalso. apply Hfinal. apply undef_decision_exists in Heq as [l_decide Hfind_dec].
+      exists (state (m ++d l_decide) f (wf_decide _ _ _ Hfind_dec Hwf)).
+      pose proof (find_decision_decomp m f l_decide Hfind_dec) as [c_decide [Hfind_undef_l Hc_in_f]].
+      eapply (t_decide m f c_decide l_decide).
+      * left. now apply find_undef_l_in_c in Hfind_undef_l.
+      * assumption.
+      * now apply find_undef_l_undef in Hfind_undef_l.
   - intros Hsat. unfold Sat in Hsat. unfold Model in Hsat. destruct Hsat as [m Heq].
     remember (normalize m f) as m' eqn:H.
     pose proof (normalize_only_dec m f) as Honly_dec.
