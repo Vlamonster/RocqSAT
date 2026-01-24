8695737ebb9d184ad403b25c28167cf16ecabb5d
Julia Dijkstra <julia.dijkstra@technolution.nl>
2026-01-24 01:38:40 +0100

    Prove `derivation_entails`

--- code/verification/Entails.v
+++ code/verification/Entails.v
@@ -1,4 +1,4 @@
-From Stdlib Require Import List.
+From Stdlib Require Import List Relations.
 Import ListNotations.
 From RocqSAT Require Import Lit CNF Evaluation WellFormed Trans.
 
@@ -23,4 +23,16 @@ Admitted.
 
 Lemma derivation_entails: forall (m m': PA) (f: CNF) (Hwf: WellFormed m f) (Hwf': WellFormed m' f),
   state m f Hwf ==>* state m' f Hwf' -> Entails f m -> Entails f m'.
-Admitted.
+Proof.
+  intros m m' f Hwf Hwf' H. apply clos_rt_rtn1_iff in H. 
+  remember (state m f Hwf) as s eqn:Heqs.
+  remember (state m' f Hwf') as s' eqn:Heqs'.
+  generalize dependent m. generalize dependent m'.
+  induction H.
+  - intros. congruence.
+  - intros. subst s; subst z. destruct y.
+    + inversion H.
+    + apply trans_same_formula in H as Heq. subst f0. apply trans_entails in H.
+      * assumption.
+      * now apply IHclos_refl_trans_n1 with (Hwf':= Hwf0) (Hwf:=Hwf).
+Qed.
